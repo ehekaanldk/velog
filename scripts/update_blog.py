5bcabe530980150a069e271edbe8ad5a5ef06443
jihyeon <jihyeon@gmail.com>
2025-04-02 21:47:43 +0900

    test02

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -22,16 +22,11 @@ repo = git.Repo(repo_path)
 # RSS 피드 파싱
 feed = feedparser.parse(rss_url)
 
-# 각 글을 파일로 저장하고 커밋
+
 for entry in feed.entries:
-    # 파일 이름에서 유효하지 않은 문자 제거 또는 대체
-    file_name = entry.title
-    file_name = file_name.replace('/', '-')  # 슬래시를 대시로 대체
-    file_name = file_name.replace('\\', '-')  # 백슬래시를 대시로 대체
-    file_name += '.md'
+    file_name = entry.title.replace('/', '-').replace('\\', '-') + '.md'
     file_path = os.path.join(posts_dir, file_name)
 
-    # 파일이 이미 존재하지 않으면 생성
     if not os.path.exists(file_path):
         if hasattr(entry, 'content'):
             content = entry.content[0].value
@@ -45,6 +40,32 @@ for entry in feed.entries:
         with open(file_path, 'w', encoding='utf-8') as file:
             file.write(content)
 
-
-# 변경 사항을 깃허브에 푸시
+repo.git.add(all=True)
+repo.git.commit('-m', 'Update Velog posts')
 repo.git.push()
+# # 각 글을 파일로 저장하고 커밋
+# for entry in feed.entries:
+#     # 파일 이름에서 유효하지 않은 문자 제거 또는 대체
+#     file_name = entry.title
+#     file_name = file_name.replace('/', '-')  # 슬래시를 대시로 대체
+#     file_name = file_name.replace('\\', '-')  # 백슬래시를 대시로 대체
+#     file_name += '.md'
+#     file_path = os.path.join(posts_dir, file_name)
+
+#     # 파일이 이미 존재하지 않으면 생성
+#     if not os.path.exists(file_path):
+#         if hasattr(entry, 'content'):
+#             content = entry.content[0].value
+#         elif hasattr(entry, 'description'):
+#             content = entry.description
+#         elif hasattr(entry, 'summary'):
+#             content = entry.summary
+#         else:
+#             content = '본문이 없습니다.'
+
+#         with open(file_path, 'w', encoding='utf-8') as file:
+#             file.write(content)
+
+
+# # 변경 사항을 깃허브에 푸시
+# repo.git.push()
